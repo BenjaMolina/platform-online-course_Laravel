f3503a1cad546366a609f17de6dc74bcea1843c0
BenjaMolina <molinasb.94@gmail.com>
2019-02-14 00:43:05 -0600

    Navegacion basada en roles - 030.- Navegacion dinamica para estudiantes y profesores

--- resources/views/partials/navigations/teacher.blade.php
+++ resources/views/partials/navigations/teacher.blade.php
@@ -2,5 +2,7 @@
 <li><a href="#" class="nav-link">{{ __('Mis suscripciones') }}</a></li>
 <li><a href="#" class="nav-link">{{ __('Mis facturas') }}</a></li>
 <li><a href="#" class="nav-link">{{ __('Mis cursos') }}</a></li>
+<li><a href="#" class="nav-link">{{ __('Cursos desarrollados por mi') }}</a></li>
+<li><a href="#" class="nav-link">{{ __('Crear curso') }}</a></li>
 
 @include('partials.navigations.logged')
