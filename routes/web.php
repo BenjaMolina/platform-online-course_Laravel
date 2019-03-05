e34abf9a680d3a3a1ae18f2195cd342e983aa29a
BenjaMolina <molinasb.94@gmail.com>
2019-03-04 22:41:59 -0600

    Estudiante Curso (inscribirse) - 057.- Obtener los cursos a los que un estudiante esta suscrito

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'courses'], function () {
 
+    Route::get('/subscribed', 'CourseController@subscribed')->name('courses.subscribed')->middleware('auth');
     Route::get('/{course}/inscribe', 'CourseController@inscribe')->name('courses.inscribe')->middleware('auth');
 
     Route::get('/{course}', 'CourseController@show')->name('courses.detail');
